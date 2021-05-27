896a0f6791e9f69c0416ed990c55f76b80778e7f
Ahmet Fatih Cengiz <37782582+afc-afc0@users.noreply.github.com>
2021-05-27 15:56:02 -0400

    Add files via upload

---
+++ CerasSerializer/CerasController.cs
@@ -0,0 +1,34 @@
+﻿using Ceras;
+
+namespace Basic_Chess_Server.Ceras
+{
+    class CerasController
+    {
+
+        #region Singleton
+        private static CerasController instance;
+        public static CerasController Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+        #endregion
+
+        private CerasSerializer ceras;
+
+        public CerasController()
+        {
+            ceras = new CerasSerializer();
+            instance = this;
+        }
+
+        public CerasSerializer GetCeras()
+        {
+            return ceras;
+        }
+
+    }
+
+}
